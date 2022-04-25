8ba3c4428a3ae08033c7d1f2a49c77aa7d7dadf8
draxi <lyntotto@hotmail.com>
2022-04-25 11:39:51 +0200

    ClampedArmor
    Changed it so armor absorption gets clamped based on the armorlevel.

--- game/dsq2.c
+++ game/dsq2.c
@@ -24,6 +24,11 @@ char *HighAscii(char *str)
 	return str;
 }
 
+int clamp(int i, int min, int max) {
+	const int t = i < min ? min : i;
+	return t > max ? max : t;
+}
+
 void DrinkHealth(edict_t *ent) {
 	if (!ent->client->resp.health_flask)
 		return;
